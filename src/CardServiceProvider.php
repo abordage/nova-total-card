8447755324d0d6c2f92e28b4b3a5e445f6bbffc1
Pavel Bychko <box@abordage.dev>
2024-03-04 23:54:41 +0300

    Refactor CardServiceProvider class
    This commit removes unused methods (routes, register) from CardServiceProvider. It also removes the redundant comments around the boot method and including type hinting for the return value on boot. These changes make the class lean and easier to understand.

--- src/CardServiceProvider.php
+++ src/CardServiceProvider.php
@@ -10,35 +10,10 @@ use Laravel\Nova\Nova;
 
 class CardServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('total-card', __DIR__.'/../dist/js/card.js');
         });
-    }
-
-    /**
-     * Register the card's routes.
-     *
-     * @return void
-     */
-    protected function routes()
-    {
-        //
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
